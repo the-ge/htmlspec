6021f65e15b00f1303a280453dd55b97dc2ad004
the-ge <g1704578400@tenita.eu>
2026-07-15 11:06:30 +0300

    chore(util.py): remove dead code: pairwise_last(), dict_lastitems(), list_lastitems()

--- src/util.py
+++ src/util.py
@@ -38,32 +38,6 @@ class EventHandler:
     applies_to: str
 
 
-def pairwise_last(iterable):
-    """s -> (s0,s1), (s1,s2), (s2, s3), ..., (sLast, None)"""
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return itertools.zip_longest(a, b, fillvalue=None)
-
-
-def dict_lastitems(xs):
-    """Iterates through a dict, generating a 3-tuple: `(key, value, last: bool)`
-    where `last` is True iff it is the last item in the dict iterator.
-
-    e.g. `xs -> (k0, v0, False), (k1, v1, False), ..., (kLast, vLast, True)`"""
-    keys = pairwise_last(xs.keys())
-    for key, key2 in keys:
-        yield key, xs[key], key2 is None
-
-
-def list_lastitems(xs):
-    """Iterates through a list, generating a 2-tuple: `(item, last: bool)`
-    where `last` is True iff it is the last item in the list iterator.
-
-    e.g. `xs -> (x0, False), (x1, False), ..., (xLast, True)`"""
-    for x, y in pairwise_last(xs):
-        yield x, y is None
-
-
 def grouper(iterable, n, fillvalue=None):
     """Collect data into fixed-length chunks or blocks"""
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
